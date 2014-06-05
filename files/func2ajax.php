8b7867c1bf97afbb4bacbdc42412cade058254b4
creative84 <rangeking@hotmail.com>
2014-06-05 13:33:28 +0200

    Create func2ajax.php
    Still not sure how you want to incorporate ajax into the scripts.. but here the start of a working example... ill try to keep it as simple as possible :)

---
+++ files/func2ajax.php
@@ -0,0 +1,18 @@
+<?php
+/* FUNC2AJAX
+this is a demo/example page that gets _func functions and outputs them for ajax get requests
+*/
+
+// GET FID (function ID)
+if(!isset($_GET['fid']) || !is_numeric($_GET['fid'])){
+	echo 'Error folder id missing'; exit;	
+}else{$fid = $_GET['fid'];}
+
+// INCLUDE _FUNC.PHP
+require_once('_func.php');
+
+
+if($fid == "1"){
+  echo GetQuote();
+}
+?>
