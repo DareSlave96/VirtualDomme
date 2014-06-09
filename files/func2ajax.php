735cf93e7b7f6957728ef7b5c5818282dd339e6e
creative84 <rangeking@hotmail.com>
2014-06-09 10:48:01 +0200

    Update func2ajax.php

--- files/func2ajax.php
+++ files/func2ajax.php
@@ -3,16 +3,39 @@
 this is a demo/example page that gets _func functions and outputs them for ajax get requests
 */
 
-// GET FID (function ID)
-if(!isset($_GET['fid']) || !is_numeric($_GET['fid'])){
-	echo 'Error folder id missing'; exit;	
+require_once 'validation.php';
+$validation = new validation();
+
+// GET FID
+if(!isset($_GET['fid']) || !$validation->text($_GET['fid'])){
+	echo 'Error fid missing'; exit;	
 }else{$fid = $_GET['fid'];}
+
+// FID to lowercase
+$fid = strtolower($fid);
 
 // INCLUDE _FUNC.PHP
 require_once('_func.php');
 
+// QUOTE
+if($fid == 'quote'){
+	echo quote::fetch();
 
-if($fid == "1"){
-  echo quote::fetch();
-}
+// PUN
+}else if($fid == 'pun'){
+	echo punishment::points();
+
+// REW
+}else if($fid == 'rew'){
+	echo reward::points(); 
+	
+// TAS
+}else if($fid == 'tas'){
+	echo task::points();
+	
+// NEWS
+}else if($fid == 'news'){
+	 echo news::get();
+	
+}else{echo 'Error invalid fix';}
 ?>
