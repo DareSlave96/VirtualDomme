0504a3b6538757124bde2d19f233c836ec10d4a9
creative84 <rangeking@hotmail.com>
2014-06-09 10:26:05 +0200

    Update index.php

--- files/index.php
+++ files/index.php
@@ -2,12 +2,16 @@
 	include_once('_func.php');	
 	O18();
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<link rel="stylesheet" type="text/css" href="res/style.css">
 		<title>
 			Home - Virtual Dom(me)
 		</title>
+		<script type="text/javascript" src="jquery.js"></script>
+		<script type="text/javascript" src="main_ajax.js"></script>
 	</head>
 	<body>
 		<div id="head">
