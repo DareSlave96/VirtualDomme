c7dc876baca3a9fdb540c8f83180f229a452fcad
creative84 <rangeking@hotmail.com>
2014-06-10 10:46:40 +0200

    Create sessions.php

---
+++ files/pages/sessions.php
@@ -0,0 +1,7 @@
+<?php
+	include_once('../_func.php');	
+	O18();
+?>
+
+			<h4>Sessions</h4>
+			This section of the site will be up and running soon, please be patient and try out some of the other sections.
