55be2343ec935c826e64b4fa76e06d448317bbb1
creative84 <rangeking@hotmail.com>
2014-06-10 16:33:42 +0200

    Update menu.php
    Class added for handling the buttons live (instead of pre-binding them)

--- files/pages/menu.php
+++ files/pages/menu.php
@@ -1,10 +1,10 @@
 <b><u>Menu</u></b><br>
-<a href='#home'>Home</a>
-<a href='#tasks'>Tasks</a><br>
-<a href='#punishments'>Punishments</a><br>
-<a href='#rewards'>Rewards</a><br>
-<a href='#sessions'>Sessions</a><br>
-
+<a href='#home' class="pagebtn">Home</a>
+<a href='#tasks' class="pagebtn">Tasks</a><br>
+<a href='#punishments' class="pagebtn">Punishments</a><br>
+<a href='#rewards' class="pagebtn">Rewards</a><br>
+<a href='#sessions' class="pagebtn">Sessions</a><br>
+ 
 <?php
 if ( !isset( $_COOKIE['ID'] ) ){// If user hasn't logged in add the link
 	echo "<a href='#details'>Sign In / Up</a><br>";
