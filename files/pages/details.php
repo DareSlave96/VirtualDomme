c5a66a930f993da1593c92872745828edb18ffe7
creative84 <rangeking@hotmail.com>
2014-06-12 12:41:26 +0200

    Update details.php
    now uses ajax post.... post data i send to /ajax/details.php ... check that file for more info :)
    
    (note) this ajax function is only for form's... will add one for normal links soon.

--- files/pages/details.php
+++ files/pages/details.php
@@ -13,43 +13,39 @@
 	}
 ?>
 
-<!--
-Some stuff i need to write down while its still in my head
-
-<span id="loading_sipost" style="margin-right:10px; display:none"><img src="img/loading.gif" width="30" height="30" style="vertical-align:middle;" /></span>
-<span id="saved_sipost" style="margin-right:10px; display:none"></span>
-              
-<form name="si" action="" id="si" accept-charset="utf-8" method="post" enctype="multipart/form-data">
-
-<input type="hidden" name="target" value="details">
-
-<input type="submit" name="sipost" alt="signin" class="savebutton" value="Save" />
--->
-
 			<div id="si">
 				<b>Sign In!</b><br>
 				<br>
-				<form action="details.php?do=si" method="post">
+				<form name="si" action="" id="si" accept-charset="utf-8" method="post" enctype="multipart/form-data">
+                			<input type="hidden" name="target" value="details">
 					Username: <br>
 					<input name="UN"><br>
 					Password: <br>
 					<input name="PW" type="password"><br>
-					<input type="submit">
+                    		<input type="submit" name="sipost" alt="signin" class="savebutton" value="Sign in" />
 				</form>
 			</div>
+            		<span id="loading_sipost" style="margin-right:10px; display:none"><img src="img/loading.gif" width="30" height="30" style="vertical-align:middle;" /></span>
+			<span id="saved_sipost" style="margin-right:10px; display:none"></span>
+            
+            
 			<div id="su">
 				<b>Sign Up!</b><br>
 				<br>
-				<form action="details.php?do=su" method="post">
+				<form name="su" action="" id="su" accept-charset="utf-8" method="post" enctype="multipart/form-data">
+                			<input type="hidden" name="target" value="details">
 					Username: <br>
 					<input name="UN"><br>
 					Password: <br>
 					<input name="PW1" type="password"><br>
 					Password again: <br>
 					<input name="PW1" type="password"><br>
-					<input type="submit">
+					<input type="submit" name="supost" alt="signup" class="savebutton" value="Sign ip" />
 				</form>
 			</div>
+            		<span id="loading_supost" style="margin-right:10px; display:none"><img src="img/loading.gif" width="30" height="30" style="vertical-align:middle;" /></span>
+			<span id="saved_supost" style="margin-right:10px; display:none"></span>
+            
 			<div id="fpass">
 				
 			</div>
